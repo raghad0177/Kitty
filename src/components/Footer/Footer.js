06097506864eea84488da0c5b5712755917f05cd
Mahmoud1Nassar <mhmooud456@gmail.com>
2024-05-18 23:14:50 +0000

    added modaule and renderd the data on the home page also makes some edits on the style of the webpage

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,32 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <>
-      <footer className="footer">
-        Rights Are Reserved © Powered By Masar 2024
-      </footer>
-    </>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-nav">
+          <a href="#home">Home</a>
+          <a href="#about">About Us</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </div>
+        <div className="footer-social">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+        <div className="footer-text">
+          Rights Are Reserved © Powered By Masar 2024
+        </div>
+        <button className="back-to-top" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+          Back to Top
+        </button>
+      </div>
+    </footer>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
